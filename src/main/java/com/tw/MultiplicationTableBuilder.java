1f2e2ae35d5af39d7035b63f3ba2265be7dd3525
WUKE7 <kevin.wu@oocl.com>
2024-11-18 16:28:32 +0800

    feat, create generateRow function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -7,12 +7,20 @@ public class MultiplicationTableBuilder {
         int start = 2;
         int end = 4;
         String multiplicationTable = builder.build(start, end);
-
         System.out.println(multiplicationTable);
     }
 
     public boolean checkNumber(int start, int end){
         return start >= end && start > 1 && start <= 1000 && end > 1 && end <= 1000;
+    }
+
+    public String generateRow(int start, int end){
+        String result = "";
+        for(int i = start; i < end; i++){
+            result += start + "*" + i + "=" + start * i + "\t";
+        }
+        result += start + "*" + end + "=" + start * end;
+        return result;
     }
 
     public String build(int start, int end) {
